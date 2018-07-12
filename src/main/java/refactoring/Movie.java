68e92d758437d128bd6997d94ee42d34c0190fc0
deepanar <deepanar@admins-MacBook-Pro-2.local>
2018-07-13 01:32:05 +0530

    remove dead code

--- src/main/java/refactoring/Movie.java
+++ src/main/java/refactoring/Movie.java
@@ -2,10 +2,6 @@ package refactoring;
 
 public abstract class Movie {
 
-    public static final int CHILDREN = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
-
     private String title;
 
     public Movie(String title) {
--- src/main/java/refactoring/RegularMovie.java
+++ src/main/java/refactoring/RegularMovie.java
@@ -2,7 +2,7 @@ package refactoring;
 
 public class RegularMovie extends Movie {
     private static final double DAYS_THRESHOLD = 2;
-    private static final double REGULAR_PRICE = 2;
+    private static final double BASE_PRICE = 2;
     private static final double EXTRA_DAYS_PRICE_FACTOR = 1.5;
 
     public RegularMovie(String title) {
@@ -11,7 +11,7 @@ public class RegularMovie extends Movie {
 
     @Override
     public double calculateAmount(int numberOfDaysRented) {
-        double amount = REGULAR_PRICE;
+        double amount = BASE_PRICE;
         if (numberOfDaysRented > DAYS_THRESHOLD)
             amount += (numberOfDaysRented - DAYS_THRESHOLD) * EXTRA_DAYS_PRICE_FACTOR;
         return amount;
